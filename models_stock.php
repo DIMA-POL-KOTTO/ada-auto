9ae6efbe66935f2368a4652b203dc0602525291c
Dima Polubeiko <dpolubejko@gmail.com>
2025-11-05 13:37:41 +0300

    title in models_stock.php

--- models_stock.php
+++ models_stock.php
@@ -9,7 +9,7 @@ $brands = $pdo->query("SELECT DISTINCT brand FROM car_in_stock ORDER BY brand")-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Модельный ряд - ADA Auto</title>
+    <title>Авто в наличии - ADA Auto</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="styles/style_base.css">
