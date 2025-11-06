119b05cd579ee97de2e7154c56e0e1c2f16fc8b9
Dima Polubeiko <dpolubejko@gmail.com>
2025-11-06 23:33:02 +0300

    remake cars_popular

--- lib/popular_models.php
+++ lib/popular_models.php
@@ -2,16 +2,17 @@
 require 'db_connect.php'; 
 $sql = "
     SELECT 
-    cm.*, 
-    cmp.badge_type,
-    cmp.description
-    FROM car_models_popular cmp
-    JOIN car_in_stock cm ON cmp.car_model_id = cm.id
-    WHERE cm.in_stock = 1
-    ORDER BY cmp.sort_order ASC, cm.brand ASC
+    cis.*,
+        cm.brand,
+        cm.model,
+        cp.badge_type,
+        cp.description
+    FROM cars_popular cp
+    JOIN cars_in_stock cis ON cp.car_in_stock_id = cis.id
+    JOIN car_models cm ON cis.model_id = cm.id
+    ORDER BY cp.sort_order ASC
 ";
 
 $stmt = $pdo->query($sql);
 $popular_models = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-<!-- Подключить models_popular -->
